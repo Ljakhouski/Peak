caec91e1fc784816d1223db7cdacb4aa7cb5f973
Ljachovsky <aleksandr.ru.www@gmail.com>
2020-08-04 02:17:03 +0300

    added project files

---
+++ PeakC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Peak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
