6d3f7af322c78ed72d94fd51b7ac0364fb1ac3a3
ljakhouski <aleksandr.ru.www@gmail.com>
2022-08-24 03:43:58 +0300

    if while and comparison added

---
+++ PeakC/Generation/X86_64/BooleanLogic.cs
@@ -0,0 +1,69 @@
+﻿namespace Peak.PeakC.Generation.X86_64
+{
+    static class BooleanLogic
+    {
+        // can take bool-type-expession or comparison-expression (boolean logical and > < >= <= != =)
+        // in both cases need to make cast expression to conditional jump ( <bool_expr> == true? jmp trueLabel )
+        public static GenResult Generate(BinaryNode node, SymbolTable st, string trueLabel, string falseLabel) 
+        {
+
+            if (node.Operator.Content == "and")
+            {
+                var leftExprIsTrueLabel = IdGenerator.GenerateLabel();
+                //TODO: cast boolean expr 
+                CastBoolToCompareAndGenerate(node.Left, st, leftExprIsTrueLabel, falseLabel);
+                st.Emit($"{leftExprIsTrueLabel}:");
+                CastBoolToCompareAndGenerate(node.Right, st, trueLabel, falseLabel);
+            }
+            else if (node.Operator.Content == "or")
+            {
+                var rightExprIsFalseLabel = IdGenerator.GenerateLabel();
+
+                CastBoolToCompareAndGenerate(node.Left, st, trueLabel, rightExprIsFalseLabel);
+                st.Emit($"{rightExprIsFalseLabel}:");
+                CastBoolToCompareAndGenerate(node.Right, st, trueLabel, falseLabel);
+            }
+            else
+                throw new CompileException();
+
+            return new EmptyGenResult();
+        }
+
+        public static void CastBoolToCompareAndGenerate(Node node, SymbolTable st, string trueLabel, string falseLabel)
+        {
+            if (isComparisonOperation(node))
+            {
+                Comparison.Generate(node as BinaryNode, st, trueLabel, falseLabel);
+            }
+            else
+            {
+                var result = Expression.Generate(node, st);
+                if (result.ResultType.Type != Peak.PeakC.Generation.Type.Double)
+                    Error.ErrMessage(node.MetaInf, "boolean type expected");
+
+                st.MemoryAllocator.MoveToAnyRegister(result);
+                var reg = st.MemoryAllocator.GetRegister(result.ReturnDataId);
+                st.Emit($"cmp {reg}, 1");
+                st.Emit($"je {trueLabel}");
+                if (falseLabel != "")
+                    st.Emit($"jmp {falseLabel}");
+            }
+        }
+        private static bool isComparisonOperation(Node node)
+        {
+            if (node is BinaryNode)
+            {
+                var bn = node as BinaryNode;
+                if (bn.Operator == ">" ||
+                    bn.Operator == ">=" ||
+                    bn.Operator == "<" ||
+                    bn.Operator == "<=" ||
+                    bn.Operator == "!=" ||
+                    bn.Operator == "=" 
+                    )
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ PeakC/Generation/X86_64/Comparison.cs
@@ -0,0 +1,90 @@
+﻿using Peak.PeakC;
+using Peak.PeakC.Generation;
+using Peak.PeakC.Generation.X86_64;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peak.PeakC.Generation.X86_64
+{
+    static class Comparison
+    {
+        // take only int/double type
+        public static GenResult Generate(BinaryNode node, SymbolTable st, string trueLabel, string falseLabel = "")
+        {
+            var alloc = st.MemoryAllocator;
+
+            var left = Expression.Generate(node.Left, st);
+            var right = Expression.Generate(node.Right, st);
+
+            var generateType = left.ResultType.Type; // TODO: separate int or double (SSE) mode
+            checkTypes();
+
+            var left_t = alloc.MoveToAnyRegister(left);
+            var left_r = alloc.GetRegister(left_t);
+            alloc.Block(left_t);
+            var right_t = alloc.MoveToAnyRegister(right);
+            var right_r = alloc.GetRegister(right_t);
+            alloc.Block(right_r);
+
+            st.Emit($"cmp {left_r}, {right_r}");
+
+            if (node.Operator == ">")
+            {
+                st.Emit($"jg {trueLabel}");
+            }
+            else if (node.Operator == ">=")
+            {
+                st.Emit($"jge {trueLabel}");
+            }
+            else if (node.Operator == "<")
+            {
+                st.Emit($"jl {trueLabel}");
+            }
+            else if (node.Operator == "<=")
+            {
+                st.Emit($"jle {trueLabel}");
+            }
+            else if (node.Operator == "=")
+            {
+                st.Emit($"je {trueLabel}");
+            }
+            else if (node.Operator == "!=")
+            {
+                st.Emit($"jne {trueLabel}");
+            }
+            else
+                throw new CompileException();
+
+            if (falseLabel != "")
+            {
+                st.Emit($"jmp {falseLabel}");
+            }
+
+            alloc.Unblock(left_r);
+            alloc.Unblock(right_r);
+
+            return new EmptyGenResult();
+
+            void checkTypes()
+            {
+                if (left.ResultType.Type != Peak.PeakC.Generation.Type.Int
+                    ||
+                    left.ResultType.Type != Peak.PeakC.Generation.Type.Double)
+                    Error.ErrMessage(node.Left.MetaInf, "wrong expression type");
+
+                if (right.ResultType.Type != Peak.PeakC.Generation.Type.Int
+                    ||
+                    right.ResultType.Type != Peak.PeakC.Generation.Type.Double)
+                    Error.ErrMessage(node.Right.MetaInf, "wrong expression type");
+
+                if (left.ResultType != right.ResultType)
+                    Error.ErrMessage(node.Right.MetaInf, "types do not match");
+            }
+
+        }
+
+        
+
+    }
+}
